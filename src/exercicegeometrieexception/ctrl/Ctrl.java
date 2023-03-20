7f0cd1df101d1a574d9127de4df6e7d0ec7c68cd
delatenam01 <delatenam01@WSTEMFB32-06.stu.net.fr.ch>
2023-03-20 14:22:53 +0100

    Programme fonctionnel selon les demandes

--- src/exercicegeometrieexception/ctrl/Ctrl.java
+++ src/exercicegeometrieexception/ctrl/Ctrl.java
@@ -11,25 +11,45 @@ public class Ctrl implements ICtrlIhm {
     public void demarrer() {
         refIhm.demarrer();
     }
-    
+
     @Override
-    public void selectCalcRadius( String value ) {
-        double valueDouble = Double.parseDouble( value );
-        refIhm.afficheResultatCircle( String.valueOf( refWorker.calcRadiusCircleFromArea( valueDouble ) ) );
+    public void selectCalcRadius(String value) {
+        double valueDouble = 0;
+        try {
+            valueDouble = Double.parseDouble(value);
+
+        } catch (NumberFormatException e) {
+            refIhm.afficheMessage("Réécrivez votre nombre en chiffre " + value + " : Invalide");
+            refIhm.afficheResultatCircle(String.valueOf(0));
+        }
+        refIhm.afficheResultatCircle(String.valueOf(refWorker.calcRadiusCircleFromArea(valueDouble)));
     }
 
     @Override
-    public void selectCalcLargeur( String valueArea, String valueLongueur ) {
-        double valueAreaDouble = Double.parseDouble( valueArea );
-        double valueLongueurDouble = Double.parseDouble( valueLongueur );
-        refIhm.afficheResultatRectangle( String.valueOf( refWorker.calcLargeurFromRectangle( valueAreaDouble, valueLongueurDouble ) ) );
+    public void selectCalcLargeur(String valueArea, String valueLongueur) {
+        double valueAreaDouble = 0;
+        double valueLongueurDouble = 0;
+        try {
+            valueAreaDouble = Double.parseDouble(valueArea);
+        } catch (NumberFormatException e) {
+            valueAreaDouble = Double.MIN_VALUE;
+            refIhm.afficheMessage("Réécrivez votre nombre en chiffre " + valueArea + " : Invalide");
+
+        }
+        try {
+            valueLongueurDouble = Double.parseDouble(valueLongueur);
+        } catch (NumberFormatException e) {
+            valueLongueurDouble=Double.MIN_VALUE;
+            refIhm.afficheMessage("Réécrivez votre nombre en chiffre " + valueLongueur + " : Invalide");
+        }
+        refIhm.afficheResultatRectangle(String.valueOf(refWorker.calcLargeurFromRectangle(valueAreaDouble, valueLongueurDouble)));
     }
 
-    public void setRefIhm( IIhmCtrl refIhm ) {
+    public void setRefIhm(IIhmCtrl refIhm) {
         this.refIhm = refIhm;
     }
 
-    public void setRefWorker( IWrkCtrl refWorker ) {
+    public void setRefWorker(IWrkCtrl refWorker) {
         this.refWorker = refWorker;
     }
 
